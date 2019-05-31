679aa5908c3e6165da3a7050000f4f323a75a4b9
znoble360 <44977541+znoble360@users.noreply.github.com>
2019-05-30 23:37:19 -0400

    finished untested search script

--- API/searchNames.php
+++ API/searchNames.php
@@ -1,5 +1,79 @@
 <?php
+	// this script takes in two parrameters in json form:
+	// 	-ID, 	which should be the userID of the user who is currently signed in
+	// 	-search, 	which should be the name or part of the name the user wants to use to search the database
+	
+	// this script will return a json in the format of {"results":"", "error":""} 
+	// and results will be in a 2d array of contacts(rows) with columns: name, email, phone, address
+	
+	// collect input data
+	$input = json_decode(file_get_contents('php://input'), true);
+	
+	// parse input data
+	$userID = $input["ID"];
+	$searchQuery = $input["search"];
 
+	// initialize result variables
+	$searchResults = array();
+	$contact = array();
+	
+	// try to connect to database
+	$con = new sqli("localhost", "luua4y2c74pm", "@Contact4331", "Cop4331Project1");
+	if ($con->connect_error)
+	{
+		sendError($con->connect_error);
+		return;
+	}
+	
+	// creates a query to collect all the contact entries for the current user that contain "searchQuery"
+	$sql = "SELECT name, email, phone, address FROM Contacts WHERE 
+		(name LIKE '%" . $searchQuery . "%') AND (ID = '" . $userID . "')";
+	
+	$result = $con->query($sql);
+	
+	// return no results error if no results found
+	if ($result->num_rows < 1)
+	{
+		sendError("No results");
+		return;
+	}
+	
+	// loop through the hits
+	while($row = $result->fetch_assoc())
+	{
+		// add each element of the contact result
+		$contact[] = $row["name"];
+		$contact[] = $row["email"];
+		$contact[] = $row["phone"];
+		$contact[] = $row["address"];
+		
+		// adds completed contact to searchResults array
+		$searchResults[] = $contact;
+	}
+	
+	// sends the 2d searchResults array
+	sendInfo($searchResults);
+	return;
+	
+	__________________________________________________________________________________
+	
 
+	// sends a json to what called this script
+	function returnJson($json)
+	{
+		header('Content-type: application/json');
+		echo $json;
+	}
 
+	// format a json to return an error
+	function sendError($error)
+	{
+		returnJson('{"results":"", "error":"' . $error . '"}');
+	}
+
+	// format a json to return searchResults
+	function sendInfo($searchResults)
+	{
+		returnJson('{"results":[' . json_encode($searchResults) . '], "error":""}');
+	}
 ?>
