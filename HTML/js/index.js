a2777f14296ae9ca031aac86cf0f62d6d7a88c3e
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-06-04 18:38:04 -0400

    Add files via upload

--- HTML/js/index.js
+++ HTML/js/index.js
@@ -4,22 +4,3 @@ function forgotPassword()
 {
 	document.getElementById("forgotPassBtn").value = "that sucks:(";
 }
-
-
-
-
-function hideOrShow(elementId, showState)
-{
-	var vis = "visible";
-	var dis = "initial";
-	if(!showState)
-	{
-		vis = "hidden";
-		dis = "none";
-	}
-
-	document.getElementById(elementId).style.visibility = vis;
-	document.getElementById(elementId).style.display = dis;
-}
-
-console.log("Hello World");
