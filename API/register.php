192f7f55d170a3959fbc195edadba52a983abed2
travish23 <47612073+travish23@users.noreply.github.com>
2019-06-04 17:38:52 -0400

    Update register.php

--- API/register.php
+++ API/register.php
@@ -1,19 +1,69 @@
-<form id='register' action='register.php' method='post' 
-    accept-charset='UTF-8'>
-<fieldset >
-<legend>Register</legend>
-<input type='hidden' name='submitted' id='submitted' value='1'/>
-<label for='name' >Your Full Name*: </label>
-<input type='text' name='name' id='name' maxlength="50" />
-<label for='email' >Email Address*:</label>
-<input type='text' name='email' id='email' maxlength="50" />
-
-<label for='username' >UserName*:</label>
-<input type='text' name='username' id='username' maxlength="50" />
-
-<label for='password' >Password*:</label>
-<input type='password' name='password' id='password' maxlength="50" />
-<input type='submit' name='Submit' value='Submit' />
-
-</fieldset>
-</form>
+<?php
+	include("");
+	session_start();
+	$_SESSION = array();
+	
+	if($_SERVER["REQUEST_METHOD"] == "POST")
+	{
+		$username = $_POST['username'];
+		$password = $_POST['password']; 
+		$email = $_POST['email']
+		$authpassword = $_POST['password_confirm'];
+		
+		if($password != $authpassword)
+		{
+			echo "<script>";
+			echo 'alert("Passwords do not match");';
+			echo 'location = "index.html"';
+			echo "</script>";
+			exit();
+		}
+		
+		$con = new sqli("localhost", "luua4y2c74pm", "@Contact4331", "Cop4331Project1");
+		
+		if (mysqli_connect_errno($conn))
+		{
+			echo("Failed to connect to MySQL: " . mysqli_connect_error($conn));
+		}
+		
+		else
+		{
+			$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+			
+			$stmt = $conn->prepare("INSERT INTO Users (Username,Password) VALUES (?, ?)");
+			
+			$stmt->bind_param("ss", $username, $hashed_password);
+			
+			if($stmt->execute())
+			{
+				echo "<script>";
+				echo 'alert("Successful Sign Up!");';
+				echo 'location = "index.html"';
+				echo "</script>";
+			}
+			
+			else
+			{
+				echo "<script>";
+				echo 'alert("Username taken, please choose another");';
+				echo 'location = "index.html"';
+				echo "</script>";
+			}
+			
+			$stmt->close();
+			$conn->close();
+		}
+	}
+	
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+?>
