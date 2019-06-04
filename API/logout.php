89ddca1a68950c1bc6ccd9e8cb554c97fa326b4c
travish23 <47612073+travish23@users.noreply.github.com>
2019-06-04 16:10:41 -0400

    Update logout.php

--- API/logout.php
+++ API/logout.php
@@ -1,8 +1,7 @@
 <?php
 	session_start();
-	$_SESSION = array();
-	if(session_destroy()) 
-	{
-		header("Location: index.html");
-	}
+	unset($_SESSION['username']);
+	session_destroy();
+	header("Location: login.php");
+	exit;
 ?>
