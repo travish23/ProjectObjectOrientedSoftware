3ea46e609eb27edf9c5d696fe6cf2ecd04b958e3
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-06-05 18:07:28 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -10,7 +10,7 @@ function doLogin()
 	var url = 'http://contactmanager.site/ProjectObjectOrientedSoftware/API/login.php';
 	//alert("Javascript");
 	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
+	xhr.open("POST", url);
 	xhr.setRequestHeader("Content-type", 'application/json; charset=UTF-8');
 	console.log("try");
 	try
