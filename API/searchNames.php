6e2dde6e2fd5294767a0818e23c2d245bf41a935
znoble360 <44977541+znoble360@users.noreply.github.com>
2019-06-05 15:22:10 -0400

    Update searchNames.php

--- API/searchNames.php
+++ API/searchNames.php
@@ -6,7 +6,10 @@
 	// this script will return a json in the format of {"results":"", "error":""} 
 	// and results will be in a 2d array of contacts(rows) with columns: contact_id, name, email, phone, address
 	
-	var_dump($input);
+	echo "this is php";
+	
+	
+	/* var_dump($input);
 	
 	// collect input data
 	$input = json_decode(file_get_contents('php://input'), true);
@@ -79,5 +82,5 @@
 	function sendInfo($searchResults)
 	{
 		returnJson('{"results":[' . json_encode($searchResults) . '], "error":""}');
-	}
+	} */
 ?>
