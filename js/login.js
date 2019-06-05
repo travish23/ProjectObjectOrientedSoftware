e7068ab3b47d89e37ee1a37f99c600fefe6e7675
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-06-05 17:33:22 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -12,6 +12,7 @@ function doLogin()
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", 'application/json; charset=UTF-8');
+	console.log("try");
 	try
 	{
 		xhr.send(jsonPayload);
