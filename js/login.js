d0864d232a7960bdbf3af19a9527149e95f2868d
travish23 <47612073+travish23@users.noreply.github.com>
2019-06-05 17:14:22 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -27,9 +27,10 @@ function doLogin()
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", 'application/json; charset=UTF-8');
+	xhr.send(jsonPayload);
 	try
 	{
-		xhr.send(jsonPayload);
+		//xhr.send(jsonPayload);
 		xhr.onreadystatechange = function()
 		{
 		    // Complete
