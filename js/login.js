66f61c2f5a1a1b8011246f71f6a711eed6c0721b
travish23 <47612073+travish23@users.noreply.github.com>
2019-06-05 16:43:40 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -17,10 +17,10 @@ function updateApplication(event)
 // Called when submit button on login page is hit
 function doLogin()
 {
-	var username = document.getElementsByName("username")[0].value;
-	var password = document.getElementsByName("password")[0].value;
+	var username = document.getElementById("loginUser").value;
+	var password = document.getElementById("loginPass").value;
 	
-	var jsonPayload = '{"username" : "' + username + '", "password" : "' + password + '"}';
+	var jsonPayload = '{"username" : "' + loginUser + '", "password" : "' + loginPass + '"}';
 	
 	var url = 'https://contactmanager.site/login.php';
 	//alert("Javascript");
