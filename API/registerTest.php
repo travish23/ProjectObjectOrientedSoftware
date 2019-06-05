4318ccdee1639a06f0203e62b3573a6f60a83424
znoble360 <44977541+znoble360@users.noreply.github.com>
2019-06-04 20:44:31 -0400

    Create registerTest.php

---
+++ API/registerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+	// collect input data
+	$input = json_decode(file_get_contents('php://input'), true);
+	
+	// parse input data
+	$user = $input["username"];
+	$pass = $input["password"];
+	
+	$con = new sqli("localhost", "luua4y2c74pm", "@Contact4331", "Cop4331Project1");
+	if ($con->connect_error)
+	{
+		sendError($con->connect_error);
+		return;
+	}
+	
+	$sql = "INSERT INTO Users name, password VALUES user, pass";
+	
+	$result = $con->query($sql);
+
+?>
