bbc946930239cb28f7d3cd9cb9c64d6c72fb06aa
travish23 <47612073+travish23@users.noreply.github.com>
2019-06-06 08:40:22 -0400

    Update login.php

--- API/login.php
+++ API/login.php
@@ -5,47 +5,61 @@
     //var_dump(obj);
 	if($_SERVER["REQUEST_METHOD"] == "POST")
 	{
-
+	    
 		$usernameEntry = $obj["username"];
 		$passwordEntry = $obj["psw"];
     
-
-		$conn = new mysqli("localhost", "luua4y2c74pm", "@Contact4331", "Cop4331Project1");
-
+		$con = new mysqli("localhost", "luua4y2c74pm", "@Contact4331", "Cop4331Project1");
+		
 		// Bad Connection to the DB
 		if(mysqli_connect_errno($conn))
 		{
 			echo("Failed to connect to MySQL: " . mysqli_connect_error($conn));
-		}
+		} 
 		// Successful connection to DB
 		else
-		{
-			$stmt = $conn->prepare("SELECT name, password FROM Users WHERE name = ?");
-			$stmt->bind_param("s", $usernameEntry);
-
-			$stmt->execute();
-
-			$stmt->bind_result($usernameDB, $passwordDB);
-			$stmt->store_result();
-
+		{		
+			
+			
+			$sql = ("SELECT * from Users where name = '$usernameEntry' and password = '$passwordEntry'");
+			$row = mysql_fetch_array($sql);
+			
+			if($row['username']==$usernameEntry && $row['password'] == $passwordEntry){
+				
+				echo("Login success");
+			}
+			else{
+				echo("Failed login");
+				
+			}
+			
+			//$stmt = $conn->prepare("SELECT name, password FROM Users WHERE name = ?");
+			
+			//$stmt->bind_param("s", $usernameEntry);
+			
+			//$stmt->execute();
+						
+			//$stmt->bind_result($usernameDB, $passwordDB);
+			//$stmt->store_result();
+			
 			// Username doesn't exist
 			if ($stmt->num_rows() < 1)
 			{
 				// Send JSON response
 			    $response = new \stdClass();
 			    $response->username = $usernameDB;
-			    $response->password= $passwordDB;
+			    $response->psw= $passwordDB;
 			    $response->state = 2;
 			    echo json_encode($response);
 				exit();
 			}
-
+			
 			else
 			{
-				while ($stmt->fetch())
+				if ($row['username'] == $usernameEntry)
 				{
 				    // Correct password
-					if (password_verify($passwordEntry, $passwordDB))
+					if (password_verify($passwordEntry, $row['password']))
 					{
 					    // Send JSON response
 					    $response = new \stdClass();
@@ -54,7 +68,7 @@
         			    $response->state = 1;
         			    echo json_encode($response);
 
-
+        			    
 						$_SESSION['login_user'] = $usernameDB;
 						//header("location: contact_manager.php");
 					}
@@ -64,7 +78,7 @@
 						// Send JSON response
 					    $response = new \stdClass();
         			    $response->username = $usernameDB;
-        			    $response->password= $passwordDB;
+        			    $response->psw= $passwordDB;
         			    $response->state = 3;
         			    echo json_encode($response);
 					}
