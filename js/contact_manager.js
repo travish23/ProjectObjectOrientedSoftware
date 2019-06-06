6704a274627e148c3b22c5df6c6cbca6255fcea5
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-06-06 02:13:40 -0400

    Update contact_manager.js

--- js/contact_manager.js
+++ js/contact_manager.js
@@ -278,23 +278,23 @@ function clearTable()
 			table.deleteRow(i);
 	}
 
-	for (var i = 1; i <= 14; i++)
-	{
-		var row = table.insertRow(i);
-		row.classList.add("blankRow");
-
-		var cell0 = row.insertCell(0);
-		var cell1 = row.insertCell(1);
-		var cell2 = row.insertCell(2);
-		var cell3 = row.insertCell(3);
-		var cell4 = row.insertCell(4);
-
-		cell0.innerHTML = "";
-		cell1.innerHTML = "";
-		cell2.innerHTML = "";
-		cell3.innerHTML = "";
-		cell4.innerHTML = "";
-	}
+	// for (var i = 1; i <= 14; i++)
+	// {
+	// 	var row = table.insertRow(i);
+	// 	row.classList.add("blankRow");
+	//
+	// 	var cell0 = row.insertCell(0);
+	// 	var cell1 = row.insertCell(1);
+	// 	var cell2 = row.insertCell(2);
+	// 	var cell3 = row.insertCell(3);
+	// 	var cell4 = row.insertCell(4);
+	//
+	// 	cell0.innerHTML = "";
+	// 	cell1.innerHTML = "";
+	// 	cell2.innerHTML = "";
+	// 	cell3.innerHTML = "";
+	// 	cell4.innerHTML = "";
+	// }
 }
 
 
