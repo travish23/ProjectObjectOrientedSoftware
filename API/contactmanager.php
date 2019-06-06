7767e41fa1b10bad2ea51f3e339aa1c41168dc23
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-06-06 12:38:53 -0400

    Update contactmanager.php

--- API/contactmanager.php
+++ API/contactmanager.php
@@ -1,11 +1,11 @@
 <?php
     include("config.php");
-    include("displayAllContactsForUser.php");
+    //include("displayAllContactsForUser.php");
     session_start();
     $input = file_get_contents('php://input');
     $_SESSION['user_id'] = $input;
 
-    if(!isset($_SESSION['login_user']))
+    if(isset($_SESSION['user_id']))
     {
         header("location: ../HTML/userContactsPage.html");
         die();
