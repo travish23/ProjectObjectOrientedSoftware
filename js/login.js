b9746592322043740a0dfd8236f8a4fa7230321c
travish23 <47612073+travish23@users.noreply.github.com>
2019-06-06 00:05:36 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,21 +1,3 @@
-function checkForUpdate()
-{
-    if (window.applicationCache !== undefined && window.applicationCache !== null)
-    {
-        window.applicationCache.addEventListener('updateready', updateApplication);
-    }
-}
-
-function updateApplication(event)
-{
-    if (window.applicationCache.status != 4) return;
-    window.applicationCache.removeEventListener('updateready', updateApplication);
-    window.applicationCache.swapCache();
-    window.location.reload();
-}
-
-
-
 
 // Called when submit button on login page is hit
 function doLogin()
