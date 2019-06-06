b17b20e17377f89e8e8c0e7fee05e6fe9b24b903
jakeguelli <51209694+jakeguelli@users.noreply.github.com>
2019-06-06 12:31:44 -0400

    Update contact_manager.js

--- js/contact_manager.js
+++ js/contact_manager.js
@@ -249,7 +249,7 @@ function displayAllContacts(){
 	request_object.onreadystatechange = function() {
 		if(request_object.readyState === 4 && request_object.status === 200)
 		{
-			console.log("response: " + request_object);
+			console.log("response: " + request_object.responseText);
 			response_object = JSON.parse(this.responseText);
 			displayTable(response_object.results);
 			return;
