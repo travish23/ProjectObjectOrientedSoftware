89a640381134f07bab457de3a4a72a872802dc4f
travish23 <47612073+travish23@users.noreply.github.com>
2019-06-06 10:37:59 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,68 +1,44 @@
-var urlBase = 'http://contactmanager.site/ProjectObjectOrientedSoftware/API';
-var extension = "php";
 
-function doLogin(){
+// Called when submit button on login page is hit
+function doLogin()
+{
+	var username = document.getElementById("loginUser").value;
+	var password = document.getElementById("loginPass").value;
+	
+	var jsonPayload = '{"username" : "' + username + '", "password" : "' + password + '"}';
 
-	console.log("Got into sendUsernameAndPassword()");
-
-	//gets the elements given by the user and store them in variables
-	var name = document.getElementById("loginUser").value;
-	var user_password = document.getElementById("loginPass").value;
-
-	//creates an object out of those variables
-	var payload = {
-		username: name,
-		psw: user_password
-		};
-
-	//turns that object into a JSON object
-	var json_payload = JSON.stringify(payload);
-
-	console.log("The json payload is " + json_payload);
-
-	//create the XML HTTP Request object
-	var request_object = new XMLHttpRequest();
-
-	//Set the XML HTTP Request Object to send stuff to the Login.php file
-	var url = urlBase + '/testlogin.' + extension;
-
-
-	var response_object;
-
-	request_object.onreadystatechange = function() {
-		console.log("(2)The response_object is " + request_object.responseText);
-
-		if(request_object.readyState === 4 && request_object.status === 200)
+	var url = 'http://contactmanager.site/ProjectObjectOrientedSoftware/API/login.php';
+	
+	alert("Javascript");
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	//xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	//xhr.send(jsonPayload);
+	xhr.onreadystatechange = function() 
 		{
-			response_object = JSON.parse(this.responseText);
-			console.log("The parsed object is " + response_object.results[0]);
-			displayTable(response_object.results);
-			return;
+			
+		
+		    // Complete
+			if (xhr.readyState == 4 && xhr.status == 200) 
+			{
+               			 var json = JSON.parse(xhr.responseText);
+				
+			
+				// Passwords matched
+				if(json.state == 1) {
+					
+					console.log("test");
+				    window.location.href = 'http://contactmanager.site/ProjectOjectOrientedSoftware/API/contactmanager.php';
+				}
+				else{
+				    alert("Incorrect Username or Password");
+				}
+			}
 		}
-	};
-
-	request_object.open("POST", url);
-	//send the username and password to the Login.php file
-	request_object.send(json_payload);
-
-	//creates an object to get information back
-	console.log("request_object.responseText = " + request_object.responseText);
-
-	var response_object = JSON.parse(request_object.responseText);
-
-	console.log("The response object is " + request_object.responseText);
-
-	if(response_object.state == 1){
-		//updates the current user variable to the person who just logged in
-		current_user_ID = response_object.ID;
-
-		window.location.replace("http://www.contactmanager.site/ProjectObjectOrientedSoftware/HTML/userContactsPage");
-
-
-	}
-	else{
-		document.getElementById("failMessage").style.visibility = "visible";
-		document.getElementById("failMessage").style.display = "inline";
-
-	}
+		xhr.open("POST", url, true);
+		xhr.send(jsonPayload);
+		//alert(test);
+        //alert("after payload");
+        //var test = JSON.parse(xhr.response);
+        //alert("we parsed");
 }
